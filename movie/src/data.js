decb639d0427aac719d0bab7dd82519ed23d89a0
Sajdemon <xxxsajmonxxx@gmail.com>
2024-11-01 10:22:53 +0100

    first commit

---
+++ movie/src/data.js
@@ -0,0 +1,85 @@
+const allMovies = [
+  {
+    id: 1,
+    image: "/images/movie1.png",
+    title: "Enola Holmesová 2",
+    age: "+13",
+    tags: "Vtipný * Napínavý * Rodina",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "komedie",
+  },
+  {
+    id: 2,
+    image: "/images/movie2.png",
+    title: "Enola Holmesová",
+    age: "+13",
+    tags: "Vtipný * Napínavý * Rodina",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "komedie",
+  },
+  {
+    id: 3,
+    image: "/images/movie3.png",
+    title: "Rocknrolla",
+    age: "+16",
+    tags: "Gangster * Napínavý * Násilí",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "romantický",
+  },
+  {
+    id: 4,
+    image: "/images/movie4.png",
+    title: "Patriot",
+    age: "+16",
+    tags: "Gangster * Napínavý * Násilí",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "romantický",
+  },
+
+  {
+    id: 5,
+    image: "/images/movie5.png",
+    title: "Telefon pana Harrigana",
+    age: "+16",
+    tags: "Gangster * Napínavý * Násilí",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "akční",
+  },
+  {
+    id: 6,
+    image: "/images/movie6.png",
+    title: "Stážista",
+    age: "+16",
+    tags: "Rodinný * Napínavý * Romantický",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "akční",
+  },
+  {
+    id: 7,
+    image: "/images/movie7.png",
+    title: "Život",
+    age: "+13",
+    tags: "Vesmír * Napínavý * Nebezpečí",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "horor",
+  },
+  {
+    id: 8,
+    image: "/images/movie8.png",
+    title: "Kód za miliardu dolarů",
+    age: "+13",
+    tags: "IT * Napínavý",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+    category: "horor",
+  },
+];
+
+export default allMovies;
